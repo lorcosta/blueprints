0e6b37a1b62861300a567e52ee4ce23c090a028d
Lorenzo Costa <costa.lorenzo98@gmail.com>
2020-09-29 11:12:01 +0200

    prove

---
+++ venv/ex1.py
@@ -0,0 +1,4 @@
+name = raw_input("Write your first name:")
+surname = raw_input("Write your last name:")
+birth = raw_input("Write your birth date in format YYYY/DD/MM:")
+print "You are {}, {}. Born {}".format(surname, name, birth)
---
+++ venv/prov.py
@@ -0,0 +1,2 @@
+my_input = raw_input("Write something...")
+print "your input is {}".format(my_input)

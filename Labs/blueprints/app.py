002c3f696f3221aa87033d7224910ddd9b7079f7
Lorenzo Costa <costa.lorenzo98@gmail.com>
2020-12-07 16:41:02 +0100

    commit base blueprints tests

--- Labs/blueprints/app.py
+++ Labs/blueprints/app.py
@@ -1,11 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, abort
+from jinja2 import TemplateNotFound
+from simple_page import simple_page
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app.register_blueprint(simple_page)
 
 
 if __name__ == '__main__':
